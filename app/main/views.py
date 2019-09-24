e1dfeb1bcf0b8d306e9c78ce63498ded2034dc0d
UmuhireAnuarithe <anuarithemuhire@gmail.com.com>
2019-09-24 16:13:22 +0200

    add updates

--- app/main/views.py
+++ app/main/views.py
@@ -1,62 +1,110 @@
-
-from flask import render_template,request,redirect,url_for,abort,flash
+from flask import render_template,request,redirect,url_for,abort, flash
 from . import main
-from .forms import PitchForm,UpdateProfile
-from ..models import User,Pitch,Comment
-from .. import db,photos
-import markdown2
 from flask_login import login_required, current_user
-import datetime
-# CommentForm,
+from ..models import Pitch, User,Comment,Upvote,Downvote
+from .forms import PitchForm, CommentForm, UpvoteForm
+from flask.views import View,MethodView
+from .. import db
+import markdown2
 
 
-@main.route('/')
-@login_required
+# Views
+@main.route('/', methods = ['GET','POST'])
 def index():
-    """
+
+    '''
     View root page function that returns the index page and its data
-    """
-    title = 'Elevator Pitches'
-    product_pitch = Pitch.query.filter_by(category = 'Product Pitch').all()
-    pickup_lines = Pitch.query.filter_by(category = 'Pickup Lines').all()
-    interview_pitch = Pitch.query.filter_by(category = 'Interview Pitch').all()
-    promotion_pitch = Pitch.query.filter_by(category = 'Promotion Pitch').all()
+    '''
+    pitch = Pitch.query.filter_by().first()
+    title = 'elevator pitch'
+    pickuplines = Pitch.query.filter_by(category="pickuplines")
+    interviewpitch = Pitch.query.filter_by(category = "interviewpitch")
+    promotionpitch = Pitch.query.filter_by(category = "promotionpitch")
+    productpitch = Pitch.query.filter_by(category = "productpitch")
 
-    return render_template('index.html', title = title,product_pitch = product_pitch, pickup_lines = pickup_lines, interview_pitch = interview_pitch, promotion_pitch = promotion_pitch)
+    upvotes = Upvote.get_all_upvotes(pitch_id=Pitch.id)
+    
+
+    return render_template('home.html', title = title, pitch = pitch, pickuplines=pickuplines, interviewpitch= interviewpitch, promotionpitch = promotionpitch, productpitch = productpitch, upvotes=upvotes)
+    
 
 
-@main.route('/pitch/new_pitch', methods = ['GET','POST'])
+
+
+@main.route('/pitches/new/', methods = ['GET','POST'])
 @login_required
 def new_pitch():
-    pitch_form = PitchForm()    
-
-    if pitch_form.validate_on_submit():
-        pitch = Pitch(title = pitch_form.title.data, category = pitch_form.category.data, pitch_content = pitch_form.pitch_content.data, author = pitch_form.author.data)
-
-        db.session.add(pitch)
-        db.session.commit() 
+    form = PitchForm()
+    my_upvotes = Upvote.query.filter_by(pitch_id = Pitch.id)
+    if form.validate_on_submit():
+        description = form.description.data
+        title = form.title.data
+        owner_id = current_user
+        category = form.category.data
+        print(current_user._get_current_object().id)
+        new_pitch = Pitch(owner_id =current_user._get_current_object().id, title = title,description=description,category=category)
+        db.session.add(new_pitch)
+        db.session.commit()
+        
         
         return redirect(url_for('main.index'))
-    
-    return render_template('new_pitch.html', pitch_form = pitch_form)   
+    return render_template('pitches.html',form=form)
 
-@main.route('/pitch/comments', methods = ['GET', 'POST'])
+
+
+
+@main.route('/comment/new/<int:pitch_id>', methods = ['GET','POST'])
 @login_required
-def comments():    
-    comments_form = CommentsForm() 
-    comments = Comment.query.all()    
+def new_comment(pitch_id):
+    form = CommentForm()
+    pitch=Pitch.query.get(pitch_id)
+    if form.validate_on_submit():
+        description = form.description.data
 
-    if comments_form.validate_on_submit():       
+        new_comment = Comment(description = description, user_id = current_user._get_current_object().id, pitch_id = pitch_id)
+        db.session.add(new_comment)
+        db.session.commit()
 
-        # Updated comment instance
-        new_comment = Comment(body = comments_form.body.data)
 
-        # Save review method
-        new_comment.save_comment()
+        return redirect(url_for('.new_comment', pitch_id= pitch_id))
 
-        return redirect(url_for('main.comments'))
+    all_comments = Comment.query.filter_by(pitch_id = pitch_id).all()
+    return render_template('comments.html', form = form, comment = all_comments, pitch = pitch )
+
+
+@main.route('/pitch/upvote/<int:pitch_id>/upvote', methods = ['GET', 'POST'])
+@login_required
+def upvote(pitch_id):
+    pitch = Pitch.query.get(pitch_id)
+    user = current_user
+    pitch_upvotes = Upvote.query.filter_by(pitch_id= pitch_id)
     
-    return render_template('comments.html', comments_form = comments_form, comments = comments)
+    if Upvote.query.filter(Upvote.user_id==user.id,Upvote.pitch_id==pitch_id).first():
+        return  redirect(url_for('main.index'))
+
+
+    new_upvote = Upvote(pitch_id=pitch_id, user = current_user)
+    new_upvote.save_upvotes()
+    return redirect(url_for('main.index'))
+
+
+@main.route('/pitch/downvote/<int:pitch_id>/downvote', methods = ['GET', 'POST'])
+@login_required
+def downvote(pitch_id):
+    pitch = Pitch.query.get(pitch_id)
+    user = current_user
+    pitch_downvotes = Downvote.query.filter_by(pitch_id= pitch_id)
+    
+    if Downvote.query.filter(Downvote.user_id==user.id,Downvote.pitch_id==pitch_id).first():
+        return  redirect(url_for('main.index'))
+
+
+    new_downvote = Downvote(pitch_id=pitch_id, user = current_user)
+    new_downvote.save_downvotes()
+    return redirect(url_for('main.index'))
+
+
+		
 
   #***************************************************************************#
 
