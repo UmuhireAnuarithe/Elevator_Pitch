fed82a8c82cbbd40d377cbaed2b6a3a790bacace
UmuhireAnuarithe <anuarithemuhire@gmail.com.com>
2019-09-24 16:15:05 +0200

    add updates

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,23 +1,24 @@
 
-
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField,RadioField,SelectField
-from wtforms.validators import Required
+from wtforms import StringField,PasswordField,SubmitField,BooleanField,SubmitField,TextAreaField,RadioField
+from wtforms.validators import Required,Email,EqualTo
+from wtforms import ValidationError
 
-
-class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Tell us about you.',validators = [Required()])
-    submit = SubmitField('Submit')
 
 
 class PitchForm(FlaskForm):
-    title = StringField('Pitch Title', validators=[Required()])
-    author = StringField('Author', validators=[Required()])
-    pitch_content = TextAreaField('Write Pitch', validators=[Required()])  
-    category = RadioField('Pick Category', choices=[('Pickup Lines', 'Pickup Lines'), ('Interview Pitch', 'Interview Pitch'), ('Product Pitch', 'Product Pitch'), ('Promotion Pitch', 'Promotion Pitch')], validators=[Required()])  
-    submit = SubmitField('Submit')
+	title = StringField('Title', validators=[Required()])
+	description = TextAreaField("What would you like to pitch ?",validators=[Required()])
+	category = RadioField('Label', choices=[ ('promotionpitch','promotionpitch'), ('interviewpitch','interviewpitch'),('pickuplines','pickuplines'),('productpitch','productpitch')],validators=[Required()])
+	submit = SubmitField('Submit')
 
-    #categoryForm
-class CategoryForm(FlaskForm):
-    name = TextAreaField('Category')
-    submit = SubmitField()
+class CommentForm(FlaskForm):
+	description = TextAreaField('Add comment',validators=[Required()])
+	submit = SubmitField()
+
+class UpvoteForm(FlaskForm):
+	submit = SubmitField()
+
+
+class Downvote(FlaskForm):
+	submit = SubmitField()
